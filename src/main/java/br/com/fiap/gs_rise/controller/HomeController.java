91736f62059b77c1e8a7a3825d2915a2005a7129
raphtatto <raphaelatatto@gmail.com>
2025-11-20 22:42:29 -0300

    api ok, mas necessario ajustar erros de requisição

--- src/main/java/br/com/fiap/gs_rise/controller/HomeController.java
+++ src/main/java/br/com/fiap/gs_rise/controller/HomeController.java
@@ -1,13 +1,13 @@
 package br.com.fiap.gs_rise.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String redirectToSwagger() {
         return "redirect:/swagger-ui/index.html";
     }
 }
